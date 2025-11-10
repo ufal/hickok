dac7dfc333d2c9b3134db9f61b372cb8cac4db0a
Dan Zeman <zeman@ufal.mff.cuni.cz>
2025-11-10 15:14:09 +0100

    Přidán skript na hledání slov s více různými analýzami.

---
+++ tools/survey_ambiguous_analyses.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/env perl
+# Surveys morphological annotation in a CoNLL-U corpus. Focuses on word forms that have multiple different analyses.
+# Copyright © 2025 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# License: GNU GPL
+
+use utf8;
+use open ':utf8';
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+binmode(STDERR, ':utf8');
+
+my %stats;
+my $n = 0; # corpus size
+while(<>)
+{
+    # We are only interested in morphosyntactic words (tree nodes). Ignore
+    # comment lines, multiword token lines and abstract nodes.
+    if(m/^[0-9]+\t/)
+    {
+        my @f = split(/\t/);
+        my $lform = lc($f[1]);
+        $stats{nocc}{$lform}++;
+        # Take lemma, UPOS, and features.
+        my $analysis = "$f[2]\t$f[3]\t$f[5]";
+        $stats{analyses}{$lform}{$analysis}++;
+        $n++;
+    }
+}
+# Process the statistics.
+# For each word, get the number of distinct analyses observed with it.
+my @lforms = sort(keys(%{$stats{analyses}}));
+foreach my $lform (@lforms)
+{
+    my @analyses = keys(%{$stats{analyses}{$lform}});
+    $stats{nanal}{$lform} = scalar(@analyses);
+}
+# Filter the words: Keep those that have more than one analysis.
+my @amblforms = grep {$stats{nanal}{$_} > 1} (@lforms);
+# Most frequent ones first, then most ambiguous ones first, then alphabetically.
+@amblforms = sort
+{
+    my $r = $stats{nocc}{$b} <=> $stats{nocc}{$a};
+    unless($r)
+    {
+        $r = $stats{nanal}{$b} <=> $stats{nanal}{$a};
+        unless($r)
+        {
+            $r = $a cmp $b;
+        }
+    }
+    $r
+}
+(@amblforms);
+# Print the statistics.
+foreach my $lform (@amblforms)
+{
+    print("$lform\t$stats{nocc}{$lform} occurrences\t$stats{nanal}{$lform} analyses\n");
+    my @analyses = keys(%{$stats{analyses}{$lform}});
+    @analyses = sort
+    {
+        my $r = $stats{analyses}{$lform}{$b} <=> $stats{analyses}{$lform}{$a};
+        unless($r)
+        {
+            $r = $a cmp $b;
+        }
+        $r
+    }
+    (@analyses);
+    foreach my $analysis (@analyses)
+    {
+        print("\t$stats{analyses}{$lform}{$analysis}\t$analysis\n");
+    }
+    print("\n");
+}
